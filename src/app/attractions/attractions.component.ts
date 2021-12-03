571e57f02d40eb0a3192cdca3820670ead59d805
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-03 13:48:41 +0600

    Переход по компонентам программным способом на кнопку log in в компоненте home. Изменение стилей в разметке

--- src/app/attractions/attractions.component.ts
+++ src/app/attractions/attractions.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attractions',
   templateUrl: './attractions.component.html',
   styleUrls: ['./attractions.component.css']
 })
-export class AttractionsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AttractionsComponent { }
